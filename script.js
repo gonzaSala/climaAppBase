a3932b3657a0440cd588cb2b2404ee04995d13f8
gonzaSala <gonzasalamendi@gmail.com>
2023-09-09 12:07:15 -0300

    agrego que al presionar enter se realice la búsqueda

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
 let urlBase = 'https://api.openweathermap.org/data/2.5/weather'
 let api_key = '7ff9d9cbc57e623b505e441ad526179c'
 let difKelvin = 273.15;
-const loaders = document.getElementById('loader');
-loaders.style.display = 'none'
+const ciudadInput = document.getElementById('ciudadEntrada');
 
-
+ciudadInput.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        const ciudad = ciudadInput.value;
+        if (ciudad) {
+            fetchDatosClima(ciudad);
+        }
+    }
+});
 
 document.getElementById('botonBusqueda').addEventListener('click', () => {
     const ciudad = document.getElementById('ciudadEntrada').value
-    if (ciudad) {
+    if(ciudad ){
         fetchDatosClima(ciudad)
-    }
-    else {
-        // Mostrar un mensaje de error en lugar de realizar la búsqueda
-        mostrarError("Por favor, ingrese una ciudad válida.");
     }
 })
 
-function fetchDatosClima(ciudad) {
-    loaders.style.display = 'block'
+function fetchDatosClima(ciudad){
     fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
-        .then(response => {
-            if (!response.ok) {
-                mostrarError("No se encontraron datos para la ciudad ingresada.");
-                loaders.style.display = 'none'; 
-                throw new Error("No se encontraron datos para la ciudad ingresada.");
-            }
-            return response.json();
-        })
-        .then(data => mostrarDatosClima(data))
-        .catch(error => {
-            console.error(error); 
-            loaders.style.display = 'none'; 
-        });
+    .then(data => data.json())
+    .then(data => mostrarDatosClima(data))
 }
 
-
-function mostrarDatosClima(data) {
-    loaders.style.display = 'none'
+function mostrarDatosClima(data){
     const divDatosClima = document.getElementById('datosClima')
-    divDatosClima.innerHTML = ''
+    divDatosClima.innerHTML=''
+
     const ciudadNombre = data.name
     const paisNombre = data.sys.country
     const temperatura = data.main.temp
@@ -47,19 +37,17 @@ function mostrarDatosClima(data) {
     const descripcion = data.weather[0].description
     const icono = data.weather[0].icon
 
-
     const ciudadTitulo = document.createElement('h2')
     ciudadTitulo.textContent = `${ciudadNombre}, ${paisNombre}`
 
     const temperaturaInfo = document.createElement('p')
-    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura - difKelvin)}ºC`
-
+    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura-difKelvin)}ºC`
+    
     const humedadInfo = document.createElement('p')
     humedadInfo.textContent = `La humedad es: ${humedad}%`
 
     const iconoInfo = document.createElement('img')
-    iconoInfo.src = `https://openweathermap.org/img/wn/${icono}@2x.png`
-
+    iconoInfo.src= `https://openweathermap.org/img/wn/${icono}@2x.png`
 
     const descripcionInfo = document.createElement('p')
     descripcionInfo.textContent = `La descripción meteorológica es: ${descripcion}`
@@ -69,11 +57,4 @@ function mostrarDatosClima(data) {
     divDatosClima.appendChild(humedadInfo)
     divDatosClima.appendChild(iconoInfo)
     divDatosClima.appendChild(descripcionInfo)
-}
-function mostrarError(mensaje) {
-    const divDatosClima = document.getElementById('datosClima');
-    divDatosClima.innerHTML = '';
-    const errorMensaje = document.createElement('p');
-    errorMensaje.textContent = mensaje;
-    divDatosClima.appendChild(errorMensaje);
 }
